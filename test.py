10f47690af76492518882c1aab2271e03e063b40
Chris Liu <liugh@rippletek.com>
2018-09-11 15:16:44 +0800

    rename puzzles directory

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import os
 from subprocess import Popen, PIPE
 
-puzzles = [x for x in os.listdir('./puzzle') if x.startswith('p')]
+puzzles = [x for x in os.listdir('./puzzles') if x.startswith('p')]
 for p in puzzles:
-    path = f'puzzle/{p}'
+    path = f'puzzles/{p}'
     with Popen(['./main.py', path], stdout=PIPE) as pr:
         out = pr.stdout.read().decode('utf-8')
         spath = path.replace('/p', '/s')
